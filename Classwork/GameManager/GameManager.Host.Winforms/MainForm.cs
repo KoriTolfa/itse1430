5cdf6a4c848adc0bea74145bb0d4c1f952396135
kori.tolfa <kori.tolfa@my.tccd.edu>
2019-02-11 21:30:59 -0600

    new

--- Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
+++ Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace GameManager.Host.Winforms
 {
     public partial class MainForm : Form
@@ -15,11 +16,32 @@ namespace GameManager.Host.Winforms
         public MainForm()
         {
             InitializeComponent();
+
+            LoadUI();
         }
 
-        private void Form1_Load( object sender, EventArgs e )
+        void LoadUI()
         {
+            Game game = new Game();
 
+            game.Name = "DOOM";
+            game.Price = 59.99M;
+            var str = game.Publisher;
+
+            Decimal.TryParse("45.99", out game.Price); //fields can be read or written just like a normal variable
         }
+
+            private void OnfileExit ( object sender, EventArgs e )
+            {
+
+            Close();
+            }
+
+        private void aboutToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            MessageBox.Show("Help");
+        }
+    
     }
+    
 }
--- Classwork/GameManager/GameManager.Host.Winforms/Program.cs
+++ Classwork/GameManager/GameManager.Host.Winforms/Program.cs
@@ -4,19 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GameManager.Host.Winforms
+namespace GameManager
 {
-    static class Program
+    /// <summary>Represents a game.</summary> //internal is the default for accesbility
+    public class Game
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        /// <summary>Name of the game.</summary>
+       // public string Name= ""; //how you declare a field, fields have an optional acces modifier, a field is just a variable
+        public string Name
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            get { return _name; }
+            set {_name = value } //vale is a keyword in the context of set 
+
+        }
+        private string _name = " ";
+        public string publisher
+        {
+            get { return _Publisher; }
+            set { _Publisher = value; }
+           
+        }
+        /// <summary>Publisher of the game.</summary>
+        /////prperties look like data but dont store data
+        ///
+        private string _Publisher ="";
+        public decimal Price
+        {
+            get { return _Price; }
+            set { _Price = value; }
+        }
+        private decimal _Price;
+        public bool Owned;
+        public bool Completed;
+
+        public bool Validate()
+        {
+            //Name is required
+            if (String.IsNullorEmpty(Name))
+                return false;
+                
+                //price>=0
+                
+            if (Price< 0)
+             return false;
+
+            return true;
         }
     }
 }
