a69832e9ac395a5b7fd243c59b18d7be7768f38e
kori.tolfa <kori.tolfa@my.tccd.edu>
2019-02-13 21:04:40 -0600

    b

--- Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
+++ Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace GameManager.Host.Winforms
 {
     public partial class MainForm : Form
@@ -26,22 +25,110 @@ namespace GameManager.Host.Winforms
 
             game.Name = "DOOM";
             game.Price = 59.99M;
-            var str = game.Publisher;
 
-            Decimal.TryParse("45.99", out game.Price); //fields can be read or written just like a normal variable
+            var name = game.Name;
+            if (name.Length == 0)
+                /* is empty*/
+                ;
+            if (game.Name.Length == 0)
+                /* is empty */
+                ;
+
+            var isCool = game.IsCoolGame;
+            //game.IsCoolGame = false;
+
+            //Validate(game)
+            game.Validate();
+
+            //var x = 10;
+            //x.ToString();
+
+            //var str = game.Publisher;            
+            //Decimal.TryParse("45.99", out game.Price);
+
         }
 
-            private void OnfileExit ( object sender, EventArgs e )
-            {
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            //Local variable
+            var x = 10;
 
             Close();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            var form = new AboutBox();
+            form.ShowDialog();
+        }
+        private void OnSave( object sender, EventArgs e )
+        {
+            Game = SaveData();
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void OnCancel( object sender, EventArgs e )
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private decimal ReadDecimal( TextBox control )
+        {
+            if (Decimal.TryParse(control.Text, out var value))
+                return value;
+            return -1;
+        }
+
+        private void LoadData( Game game )
+        {
+
+            _txtName.Text = game.Name;
+            _txtPublisher.Text = game.Publisher;
+            _txtPrice.Text = game.Price.ToString();
+            _cbowned.Checked = game.Owned;
+            _cbCompleted.Checked = game.Completed;
+
+        }
+        private Game SaveData()
+        {
+            var game = new Game();
+            game.Name = _txtname.Text;
+            game.Publisher = _txtPublisher.Text;
+            game.Price = ReadDecimal(_txtPrice);
+            game.Owned = _cbOwned.Checked;
+            game.Completed = _cbcompleted.checked;
+
+            return game;
             }
 
-        private void aboutToolStripMenuItem_Click( object sender, EventArgs e )
-        {
-            MessageBox.Show("Help");
+            private void GameForm_Load( object sender, EventArgs e )
+            {
+                //Init UI if editing a game
+                if (Game != null)
+                    LoadData(Game);
+            }
         }
-    
+
+        private void OnGameDelete( object sender, EventArgs e )
+        {
+            var selected = GetSelectedGame();
+            if (selected == null)
+                return;
+
+            //display conformation
+            MessageBox.Show(this, $"Are you sure you want to delete this game? {selected.Name}?","Confirm delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            //TODo Delete
+            _game = null;
+        }
+
+        private Game GetSelectedGame()
+        {
+            return _game;
+        }
     }
-    
 }
+
+
