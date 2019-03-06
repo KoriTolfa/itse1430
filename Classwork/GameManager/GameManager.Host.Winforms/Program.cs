60b20336581244105b22a2415fcaf792d6188180
KoriTolfa <46698052+KoriTolfa@users.noreply.github.com>
2019-03-06 12:58:43 -0600

    Update Program.cs

--- Classwork/GameManager/GameManager.Host.Winforms/Program.cs
+++ Classwork/GameManager/GameManager.Host.Winforms/Program.cs
@@ -1,54 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GameManager
+namespace GameManager.Host.Winforms
 {
-    /// <summary>Represents a game.</summary> //internal is the default for accesbility
-    public class Game
+    static class Program
     {
-        /// <summary>Name of the game.</summary>
-       // public string Name= ""; //how you declare a field, fields have an optional acces modifier, a field is just a variable
-        public string Name
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            get { return _name; }
-            set {_name = value } //vale is a keyword in the context of set 
-
-        }
-        private string _name = " ";
-        public string publisher
-        {
-            get { return _Publisher; }
-            set { _Publisher = value; }
-           
-        }
-        /// <summary>Publisher of the game.</summary>
-        /////prperties look like data but dont store data
-        ///
-        private string _Publisher ="";
-        public decimal Price
-        {
-            get { return _Price; }
-            set { _Price = value; }
-        }
-        private decimal _Price;
-        public bool Owned;
-        public bool Completed;
-
-        public bool Validate()
-        {
-            //Name is required
-            if (String.IsNullorEmpty(Name))
-                return false;
-                
-                //price>=0
-                
-            if (Price< 0)
-             return false;
-
-            return true;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
         }
     }
 }
