39118fd57cb49757f235c4aed8e58f1811a4f4c2
KoriTolfa <46698052+KoriTolfa@users.noreply.github.com>
2019-03-06 13:10:55 -0600

    Update AssemblyInfo.cs

--- Classwork/GameManager/GameManager.Host.Winforms/Properties/AssemblyInfo.cs
+++ Classwork/GameManager/GameManager.Host.Winforms/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Game Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kori")]
+[assembly: AssemblyCompany("Your Name")]
 [assembly: AssemblyProduct("Game Manager")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5135c5e3-bc37-420d-bd0d-bbd1ceafbaf5")]
+[assembly: Guid("20d9555f-82be-46aa-886b-286d02ec7bf9")]
 
 // Version information for an assembly consists of the following four values:
 //
